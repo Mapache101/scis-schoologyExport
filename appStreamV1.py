b31c54d13c783bdb2559d6e09cfe312696a9daaf
Mapache101 <olguin.daniel@gmail.com>
2025-03-20 10:47:58 -0400

    Update appStreamV1.py
    return math.ceil(weighted_sum / weight_sum)

--- appStreamV1.py
+++ appStreamV1.py
@@ -124,10 +124,10 @@ def process_data(df, teacher, subject, course, level):
                 weighted_sum += row[avg_col] * weight
                 weight_sum += weight
         # If at least one category has a valid score, compute the normalized weighted average.
-        if weight_sum > 0:
+        if weight_sum > 0.5:
             # Normalize and round up to remove decimals.
-            #return math.ceil(weighted_sum / weight_sum)
-            return (weighted_sum / weight_sum)            
+            3return math.ceil(weighted_sum / weight_sum)
+            return (weighted_sum / weight_sum)
         else:
             return None  # or you could return NaN
 
